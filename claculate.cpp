e0c2e9b2a525f61fc5a4e2665ea35fa31a5f893e
abdulmajedalmarghmi-alt <abdulmajedalmarghmi@gmail.com>
2026-01-23 23:51:50 +0300

    Add files via upload

---
+++ claculate.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float a,b;
+  char x;
+  
+  cout <<"Enter Number1:\t";
+       cin >>x;
+       cout<<"Enter Number2;\t";
+       cin
+       cout<<"Enter The Operator\t";
+       cin>>x;
+       
+       cout<<end1<<end1;
+       
+       cout"Result:\t";
+       
+       
+       if (x=='+') {cout<<a+b;}
+       else if (x=='-') {cout<<a_b;}
+       else if (x=='*') {cout<<a*b;}
+       else if (x=='/') {cout<<a/b;}
+       else             {cout<<"Bad Command";}
+       
+       cout<<end1;
+       
+       return 0;
+       }
+   
