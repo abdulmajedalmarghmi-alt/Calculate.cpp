1524bcfe8324f1aa745b9e9fb25754e4144470da
abdulmajedalmarghmi-alt <abdulmajedalmarghmi@gmail.com>
2026-03-02 02:03:59 +0300

    Update bbg.cpp

--- .github/workflows/bbg.cpp
+++ .github/workflows/bbg.cpp
@@ -1,2 +1,6 @@
 
-
+#include<iostream>
+using namespace std;   
+{
+  cout<<"hi";
+}                                                               int main (){                int x , y , sum;                cout<<"enter tow number : ";                cin>>x>>y;                sum=x+y;                cout<<"the number = "<<x*y<<end1;                cout<<"the sum is : "<<sum<<end 1;                getchar  ();                getchar  ();                return 0;}                
